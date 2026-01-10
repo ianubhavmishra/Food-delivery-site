3308d591b0fdd591844ad81e99f5cfdefc34116f
[ianubhavmishra] <[anubhavmishra2408@gmail.com]>
2026-01-10 21:17:03 +0530

    UI fixes.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,11 +5,12 @@ const Footer = () => {
   return (
     <div className='flex max-md:flex-col gap-4 justify-between items-start text-gray-400 max-md:mt-12 mt-28 p-10 bg-stone-800' id='contact'>
       <div className='flex flex-col gap-4 justify-between w-[40vw] max-md:w-full'>
-        <img className='h-8 w-40' src={assets.logo}/>
+        <img className='h-8 w-40' src={assets.logo} />
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut maiores error deleniti est, nesciunt sed dolorum eum, maxime magni dolores magnam laborum vero quasi! Laudantium minus accusamus dolorem dolorum aliquam, rem vitae voluptates. Delectus deserunt laborum quisquam non! Repellendus odio, dolore sapiente modi</p>
+        <p>Tomato is your go-to food delivery destination for fresh, flavorful meals made with quality ingredients and crafted by trusted kitchens. We bring you a wide variety of cuisines with fast, reliable delivery, easy ordering, and a seamless experience that satisfies every craving.
+        </p>
         <div className='flex gap-3 size-8'>
-        <img src={assets.facebook_icon}/><img src={assets.twitter_icon}/><img src={assets.linkedin_icon}/>
+          <img src={assets.facebook_icon} /><img src={assets.twitter_icon} /><img src={assets.linkedin_icon} />
         </div>
       </div>
       <div>
